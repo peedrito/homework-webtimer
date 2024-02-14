483c3a57225b2552e326fec51ada10bf5f5ac644
frog <202307550043@alunos.estacio.br>
2024-02-14 19:05:46 -0300

    Feito em: 14/02/2024

--- index.js
+++ index.js
@@ -4,15 +4,23 @@ const pause = document.querySelector('.pause');
 const timerZero = document.querySelector('.timerZero');
 
 play.addEventListener('click', function(event) {
-    alert("Cliquei em 'Iniciar'");
-});
+    let seconds = 0;
+    let minutes = 0;
+    let hours = 0;
+    const timerInterval = setInterval(function() {
+        seconds++;
+        if (seconds > 59) {
+            seconds = 0;
+            minutes++;
+        }
+        if (minutes > 59) {
+            minutes = 0;
+            hours++;
+        }
+        clock.textContent = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+    }, 1000); 
 
-pause.addEventListener('click', function(event) {
-    alert("Cliquei em 'Pausar'");
-});
-
-timerZero.addEventListener('click', function(event) {
-    alert("Cliquei em 'Zerar'");
-});
-
-console.log("danilinho aqui")
+    pause.addEventListener('click', function(event) {
+        clearInterval(timerInterval);
+    });
+}); 
